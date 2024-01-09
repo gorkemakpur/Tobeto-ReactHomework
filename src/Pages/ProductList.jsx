8f8ab334fedc0c8cbe8750052e0e4baa9edcf85b
gorkemakpur <gorkemakpur@gmail.com>
2024-01-09 15:45:11 +0300

    09.01.2024 15.45

--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -42,7 +42,6 @@ export default function ProductList() {
             <Table.HeaderCell>Stok Adeti</Table.HeaderCell>
             <Table.HeaderCell>Açıklama</Table.HeaderCell>
             <Table.HeaderCell>Kategori</Table.HeaderCell>
-            <Table.HeaderCell></Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
